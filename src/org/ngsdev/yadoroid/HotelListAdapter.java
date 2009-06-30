94900dc8f8cd6c27dcea994f413d4f21731e3a62
ngs <ngs@62f2bb70-6679-11de-a9b1-77b8bb241e6b>
2009-06-30 09:12:51 +0000

    宿一覧が表示できる

--- src/org/ngsdev/yadoroid/HotelListAdapter.java
+++ src/org/ngsdev/yadoroid/HotelListAdapter.java
@@ -1,26 +1,60 @@
 package org.ngsdev.yadoroid;
+import java.util.LinkedList;
+import java.net.URL;
+import org.ngsdev.yadoroid.Yadoroid;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.widget.BaseAdapter;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.net.Uri;
 import net.jalan.jws.search.Hotel;
 import net.jalan.jws.search.HotelSearch;
+import net.jalan.jws.search.hotel.AccessInformation;
+import net.jalan.jws.search.hotel.HotelPicture;
 public class HotelListAdapter extends BaseAdapter {
 	public HotelSearch hotelSearch;
 	public Context context;
 	public TextView hotelName;
+	public TextView minPrice;
+	public TextView catchCopy;
 	public ImageView hotelImage;
 	public HotelListAdapter(Context context,HotelSearch hotelSearch) {
+		super();
 		this.hotelSearch = hotelSearch;
 		this.context = context;
 	}
 	public View getView(int position, View convertView, ViewGroup parent) {
-		TextView tv = new TextView(context);
-		tv.setText(getItem(position).name);
-		return tv;
+		final View v = View.inflate(context,R.layout.hotel_item,null);
+		final Hotel h = getItem(position);
+		hotelName = (TextView) v.findViewById(R.id.hotel_name);
+		hotelImage = (ImageView) v.findViewById(R.id.hotel_image);
+		catchCopy = (TextView) v.findViewById(R.id.catch_copy);
+		minPrice = (TextView) v.findViewById(R.id.min_price);
+		//
+		hotelName.setText(h.name);
+		int rate = h.sampleRateFrom;
+		if(rate>0)
+			minPrice.setText(h.type+"/"+String.format(context.getString(R.string.min_price),Integer.toString(rate)));
+		else
+			minPrice.setVisibility(View.GONE);
+		String ccp = h.catchCopy;
+		if(ccp!="")
+			catchCopy.setText(ccp);
+		else
+			catchCopy.setVisibility(View.GONE);
+		
+		LinkedList<HotelPicture> picts = h.pictures;
+		Bitmap bmp = h.getImageBitmap();
+		if(bmp!=null)
+			hotelImage.setImageBitmap(bmp);
+		else
+			hotelImage.setVisibility(View.GONE);
+		return v;
 	}
 	public Hotel getItem(int position) {
 		return hotelSearch.item(position);
