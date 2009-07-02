8d4db7c14b1fb4876444d8551188fd527e63f0ce
atsn.ngs <atsn.ngs@62f2bb70-6679-11de-a9b1-77b8bb241e6b>
2009-07-02 06:56:27 +0000

    ビーコンのURLを修正

--- src/net/jalan/jws/search/Hotel.java
+++ src/net/jalan/jws/search/Hotel.java
@@ -16,7 +16,7 @@ import net.jalan.jws.search.hotel.Plan;
 import org.ngsdev.yadoroid.Yadoroid;
 public class Hotel {
 	static public final String BASE_URI = "http://ck.jp.ap.valuecommerce.com/servlet/referral?sid=2462325&pid=878274998&vc_url=";
-	static public final String BEACON_URI = "http://ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=2462325&pid=878248727";
+	static public final String BEACON_URI = "http://ad.jp.ap.valuecommerce.com/servlet/gifbanner?sid=2462325&pid=878274998";
 	static public final String TAG_NAME = "Hotel";
 	public String id;
 	public String name;
