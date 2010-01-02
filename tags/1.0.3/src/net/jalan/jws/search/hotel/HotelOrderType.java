217cb4c9786dec216dcdd9e3f41b88401f0f0f6f
atsn.ngs <atsn.ngs@62f2bb70-6679-11de-a9b1-77b8bb241e6b>
2010-01-02 00:21:31 +0000

    1.0.3 release

---
+++ tags/1.0.3/src/net/jalan/jws/search/hotel/HotelOrderType.java
@@ -0,0 +1,9 @@
+package net.jalan.jws.search.hotel;
+public class HotelOrderType {
+	public static final int SEQUENCIAL = 0;
+	public static final int KANA = 1;
+	public static final int PRICE_LOW = 2;
+	public static final int PRICE_HI = 3;
+	public static final int KUCHIKOMI = 4;
+	public static final int DEFAULT = SEQUENCIAL;
+}
---
+++ tags/1.0.3/src/net/jalan/jws/search/hotel/HotelType.java
@@ -0,0 +1,10 @@
+package net.jalan.jws.search.hotel;
+public class HotelType {
+	public static final int NONE = 0;
+	public static final int RYOKAN = 1;
+	public static final int PENSION = 2;
+	public static final int CONDMINIUM = 3;
+	public static final int HOTEL = 4;
+	public static final int PUBLIC = 5;
+}
+
