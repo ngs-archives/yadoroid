379ddd4accd56b51f714b32e25bba63032e586e4
atsn.ngs <atsn.ngs@62f2bb70-6679-11de-a9b1-77b8bb241e6b>
2010-01-02 00:19:18 +0000

    1.0.3 release

--- src/org/ngsdev/yadoroid/YadoroidMap.java
+++ src/org/ngsdev/yadoroid/YadoroidMap.java
@@ -18,8 +18,9 @@ public class YadoroidMap extends MapActivity {
 		double lat = extras==null?(double) 0:extras.getDouble(AbstractYadoroid.EXTRA_KEY_HOTEL_LAT);
     	double lng = extras==null?(double) 00:extras.getDouble(AbstractYadoroid.EXTRA_KEY_HOTEL_LNG);
     	GeoPoint pt = new GeoPoint(Double.valueOf(lat*1E6).intValue(),Double.valueOf(lng*1E6).intValue());
-		//
-		MapView map = new MapView(this, "0royquBZ8caAEmKwYbuUXg7DHysNM2Vm9TIB_mw");
+		// debug 0royquBZ8caAEmKwYbuUXg7DHysNM2Vm9TIB_mw
+		// release 0royquBZ8caByyaGpJfNdK--DV8vZlYk3PM0sRw
+		MapView map = new MapView(this, "0royquBZ8caByyaGpJfNdK--DV8vZlYk3PM0sRw");
 		map.setEnabled(true);
 		map.setBuiltInZoomControls(true);
 		map.setClickable(true);
